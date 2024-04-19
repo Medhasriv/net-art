fc72b414a3874f5841855c17ee8e6e1f0c46a7b9
Medhasriv <66755998+Medhasriv@users.noreply.github.com>
2024-04-19 19:24:25 -0400

    Add files via upload

---
+++ quilt/quiltEdit.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    $("div.sea1").click(function(){
+        $(".sea1").animate({
+            left: '+=25px',
+            height: '+=20px',
+            width: '+=20px'
+          });
+    })
+
+    $("div.sea2").click(function(){
+        $(".sea2").animate({
+            left: '+=20px',
+            height: '+=20px',
+            width: '+=20px'
+          });
+    })
+
+    $("div.sea3").click(function(){
+        $(".sea3").animate({
+            left: '+=25px',
+            height: '+=20px',
+            width: '+=20px'
+          });
+    })
+
+    $("div.sea4").click(function(){
+        $(".sea4").animate({
+            left: '+=25px',
+            height: '+=20px',
+            width: '+=20px'
+          });
+    })
+
+    $("div.sea5").click(function(){
+        $(".sea5").animate({
+            left: '+=25px',
+            height: '+=20px',
+            width: '+=20px'
+          });
+    })
+
+    $("div.whitebox").click(function(){
+        $(".whitebox").animate({
+            left: '+=25px',
+            height: '+=20px',
+            width: '+=20px'
+          });
+    })
+
+    $("div.beached").click(function(){
+        $(".beached").animate({
+            left: '+=25px',
+            height: '+=20px',
+            width: '+=20px'
+          });
+    })
+
+
+})
